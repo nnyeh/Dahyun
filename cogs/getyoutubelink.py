837ae15cf460e6caba1c691abf206924626f2307
nnyeh <emil.munne@gmail.com>
2020-11-26 01:55:52 +0200

    added a new command

---
+++ cogs/getyoutubelink.py
@@ -0,0 +1,72 @@
+import os
+import discord
+import requests
+import pyyoutube
+from discord.ext import commands
+from data import database as db
+from datetime import datetime
+
+class getyoutubelink(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["yt"])
+    async def youtube(self, ctx, *, arg=None):
+
+        now = datetime.now()
+        timestamp = now.strftime("%#H:%M:%S, %#d.%#m.%Y")
+
+        if arg is None:
+            username = db.get_user(ctx.author.id)
+
+            if username is None:
+                return await ctx.send(f"`You need to first set your Last.fm username with the command`\n```>set [your username]```")
+
+            lastfm_username = username [0][1];
+            if not lastfm_username:
+                return await ctx.send(f"`You need to first set your Last.fm username with the command`\n```>set [your username]```")
+
+            recent_tracks_params = {
+                "limit": "1",
+                "user": lastfm_username,
+                "api_key": os.getenv("LASTFM_API_KEY"),
+                "format": "json",
+                "method": "user.getRecentTracks"
+            }
+
+            r = requests.get("http://ws.audioscrobbler.com/2.0/", params=recent_tracks_params)
+            rtdata = r.json()
+            rtinfo = rtdata["recenttracks"]["track"][0]
+            artist = rtinfo["artist"]["#text"]
+            track = rtinfo["name"]
+
+            video_info_params = {
+                "part": "snippet",
+                "type": "video",
+                "maxResults": 1,
+                "q": f"{artist} {track}",
+                "key": os.getenv("YOUTUBE_API_KEY"),
+            }
+
+            r = requests.get("https://www.googleapis.com/youtube/v3/search", params=video_info_params)
+            vidata = r.json()
+        else:
+            artist, track = arg.split("|")
+            video_info_params = {
+                "part": "snippet",
+                "type": "video",
+                "maxResults": 1,
+                "q": f"{artist} {track}",
+                "key": os.getenv("YOUTUBE_API_KEY"),
+            }
+
+            r = requests.get("https://www.googleapis.com/youtube/v3/search", params=video_info_params)
+            vidata = r.json()
+
+        video_id = vidata["items"][0]["id"]["videoId"]
+        video_url = f"https://youtube.com/watch?v={video_id}"
+
+        await ctx.send(f"{video_url}")
+
+def setup(bot):
+    bot.add_cog(getyoutubelink(bot))
