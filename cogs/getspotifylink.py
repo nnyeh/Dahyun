e6f56649defb0b8cf184437c63f7ae374f181c76
nnyeh <emil.munne@gmail.com>
2021-01-05 22:10:47 +0200

    fixed command returning the most popular result, not the right one

--- cogs/getspotifylink.py
+++ cogs/getspotifylink.py
@@ -34,21 +34,37 @@ class getspotifylink(commands.Cog):
             rtdata = r.json()
             rtinfo = rtdata["recenttracks"]["track"][0]
             track = rtinfo["name"]
+            print(track)
+            print("-----------")
+            artist = rtinfo["artist"]["#text"]
 
             np = "@attr" in rtinfo and "nowplaying" in rtinfo["@attr"]
             state = f"*Now playing for {lastfm_username}*" if np else f"*Last scrobbled track for {lastfm_username}*"
 
             sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
-            results = sp.search(track, type="track", limit=1)
-            spotify_url = results["tracks"]["items"][0]["external_urls"]["spotify"]
+            sp_track_url = ""
+            results = sp.search(f"{track} {artist}", type="track", limit=50)
+            items = results["tracks"]["items"]
+            lfm_artist_name_lowercase = f"{artist.lower()}"
+
+            for sp_track in items:
+                sp_track_correct = sp_track["artists"][0]
+                sp_artist_name_lowercase = sp_track_correct["name"].lower()
+                print(sp_artist_name_lowercase)
+                print(lfm_artist_name_lowercase)
+                if lfm_artist_name_lowercase == sp_artist_name_lowercase:
+                    sp_track_url = sp_track["external_urls"]["spotify"]
+                    break
+            
+            print("------------------------")
         else:
             sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
             results = sp.search(arg, type="track", limit=1)
-            spotify_url = results["tracks"]["items"][0]["external_urls"]["spotify"]
+            sp_track_url = results["tracks"]["items"][0]["external_urls"]["spotify"]
 
             state = f"*Link requested by {ctx.author.name}#{ctx.author.discriminator}*"
 
-        await ctx.send(f"{state}\n{spotify_url}")
+        await ctx.send(f"{state}\n{sp_track_url}")
 
 def setup(bot):
     bot.add_cog(getspotifylink(bot))
