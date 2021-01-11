35273d9f2acd66a0968118ef8ba14fc1659a1b20
nnyeh <emil.munne@gmail.com>
2021-01-11 18:19:39 +0200

    removed "typing..."

--- cogs/unsetusername.py
+++ cogs/unsetusername.py
@@ -7,9 +7,8 @@ class unsetusername(commands.Cog):
 
     @commands.command()
     async def unset(self, ctx):
-        async with ctx.typing():
         
-            db.update_user(ctx.author.id, "lastfm_username", None)
+        db.update_user(ctx.author.id, "lastfm_username", None)
 
         await ctx.send(f"{ctx.author.mention}`, your Last.fm username has been unset` <a:DubuRito:773333382918569984>")
 
