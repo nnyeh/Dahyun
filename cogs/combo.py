8f22907a8af157b682453ca05f8b03abdc2adca3
nnyeh <emil.munne@gmail.com>
2021-04-04 07:54:58 +0300

    fixed a stupid mistake on making higher than 1k combos work

--- cogs/combo.py
+++ cogs/combo.py
@@ -83,9 +83,9 @@ class combo(commands.Cog):
             first_artist_album = first_artist_obj["album"]["#text"]
             first_artist_track = first_artist_obj["name"]
 
-            first_artist_combo = -1
-            first_album_combo = -1
-            first_track_combo = -1
+            first_artist_combo = 0
+            first_album_combo = 0
+            first_track_combo = 0
 
             for track in tracks: 
                 if (track["artist"]["#text"] == first_artist_name):
@@ -105,11 +105,11 @@ class combo(commands.Cog):
                 else:
                     break
 
-            second_artist_combo = ""
-            second_album_combo = ""
-            second_track_combo = ""
+            second_artist_combo = 0
+            second_album_combo = 0
+            second_track_combo = 0
 
-            if first_artist_combo or first_album_combo or first_track_combo >= 1000:
+            if first_artist_combo >= 1000 or first_album_combo >= 1000 or first_track_combo >= 1000:
                 recent_tracks_params = {
                 "limit": "1000",
                 "page": "2",
@@ -146,42 +146,116 @@ class combo(commands.Cog):
                 first_artist_album = first_artist_obj["album"]["#text"]
                 first_artist_track = first_artist_obj["name"]
 
-                second_artist_combo = 0
-                second_album_combo = 0
-                second_track_combo = 0
+                if first_artist_combo >= 1000:
+                    for track in tracks: 
+                        if (track["artist"]["#text"] == first_artist_name):
+                            second_artist_combo += 1
+                        else:
+                            break
 
-                for track in tracks: 
-                    if (track["artist"]["#text"] == first_artist_name):
-                        second_artist_combo += 1
-                    else:
-                        break
+                if first_album_combo >= 1000:
+                    for track in tracks: 
+                        if (track["album"]["#text"] == first_artist_album):
+                            second_album_combo += 1
+                        else:
+                            break
 
-                for track in tracks: 
-                    if (track["album"]["#text"] == first_artist_album):
-                        second_album_combo += 1
-                    else:
-                        break
+                if first_track_combo >= 1000:
+                    for track in tracks: 
+                        if (track["name"] == first_artist_track):
+                            second_track_combo += 1
+                        else:
+                            break
 
-                for track in tracks: 
-                    if (track["name"] == first_artist_track):
-                        second_track_combo += 1
-                    else:
-                        break
+            third_artist_combo = 0
+            third_album_combo = 0
+            third_track_combo = 0
+
+            if second_artist_combo >= 2000 or second_album_combo >= 2000 or second_track_combo >= 2000:
+                recent_tracks_params = {
+                "limit": "1000",
+                "page": "3",
+                "user": lastfm_username,
+                "api_key": os.getenv("LASTFM_API_KEY"),
+                "format": "json",
+                "method": "user.getRecentTracks"
+                }
+
+                r = requests.get("http://ws.audioscrobbler.com/2.0/", params=recent_tracks_params)
+                rtdata = r.json()
+                await asyncio.sleep(0.25)
+
+                rtinfo = rtdata["recenttracks"]["track"][0]
+                artist_name = rtinfo["artist"]["#text"]
+                album_name = rtinfo["album"]["#text"]
+                track_name = rtinfo["name"]
+
+                album_info_params = {
+                    "artist": artist_name,
+                    "album": album_name,
+                    "user": lastfm_username,
+                    "api_key": os.getenv("LASTFM_API_KEY"),
+                    "format": "json",
+                    "method": "album.getInfo"
+                }
+
+                r3 = requests.get("http://ws.audioscrobbler.com/2.0/", params=album_info_params)
+                abidata = r3.json()
+                await asyncio.sleep(0.25)
+                tracks = rtdata["recenttracks"]["track"]
+                first_artist_obj = tracks[0]
+                first_artist_name = first_artist_obj["artist"]["#text"]
+                first_artist_album = first_artist_obj["album"]["#text"]
+                first_artist_track = first_artist_obj["name"]
+
+                if second_artist_combo >= 2000:
+                    for track in tracks: 
+                        if (track["artist"]["#text"] == first_artist_name):
+                            third_artist_combo += 1
+                        else:
+                            break
+
+                if second_album_combo >= 2000:
+                    for track in tracks: 
+                        if (track["album"]["#text"] == first_artist_album):
+                            third_album_combo += 1
+                        else:
+                            break
+
+                if second_track_combo >= 2000:
+                    for track in tracks: 
+                        if (track["name"] == first_artist_track):
+                            third_track_combo += 1
+                        else:
+                            break
 
             artist_combo_text = ""
             album_combo_text = ""
             track_combo_text = ""
             no_combo_text = ""
 
+            print(first_artist_combo)
+            print(first_album_combo)
+            print(first_track_combo)
+            print("------------------")
+            print(second_artist_combo)
+            print(second_album_combo)
+            print(second_track_combo)
+            print("------------------")
+            print(third_artist_combo)
+            print(third_album_combo)
+            print(third_track_combo)
+            print("------------------")
+
             if first_artist_combo >= 2:
-                artist_combo_text = f"**Artist:** {first_artist_combo + second_artist_combo} plays in a row - **[{artist_name}]({artist_url})**\n"
+                artist_combo_text = f"**Artist:** {first_artist_combo + second_artist_combo + third_artist_combo} plays in a row - **[{artist_name}]({artist_url})**\n"
             if first_album_combo >= 2 and album_url != "":
-                    album_combo_text = f"**Album:** {first_album_combo + second_album_combo} plays in a row - **[{album_name}]({album_url})**\n"
+                    album_combo_text = f"**Album:** {first_album_combo + second_album_combo + third_album_combo} plays in a row - **[{album_name}]({album_url})**\n"
             else:
                 if first_album_combo >= 2 and album_url == "":
-                    album_combo_text = f"**Album:** {first_album_combo + second_album_combo} plays in a row - **{album_name}**\n"
+                    album_combo_text = f"**Album:** {first_album_combo + second_album_combo + third_album_combo} plays in a row - **{album_name}**\n"
             if first_track_combo >= 2:
-                track_combo_text = f"**Track:** {first_track_combo + second_track_combo} plays in a row - **[{track_name}]({track_url})**"
+                track_combo_text = f"**Track:** {first_track_combo + second_track_combo + third_track_combo} plays in a row - **[{track_name}]({track_url})**"
             if artist_combo_text == "" and album_combo_text == "" and track_combo_text == "":
                 no_combo_text = f"*No consecutive tracks found.*"
 
